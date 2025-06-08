769efd52c5bf9287938b5fc3022ef51d8d3da2a7
takboo <takboo.sf@gmail.com>
2025-06-08 10:37:22 +0800

    feature(config): add env config and override config from env support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::startup::run;
 use zero2prod::{get_configuration, get_subscriber, init_subscriber};
@@ -10,11 +9,14 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let pg_pool = PgPool::connect(configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres");
+    let pg_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind port 8080");
     run(listener, pg_pool)?.await
 }
