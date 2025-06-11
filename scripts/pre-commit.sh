3405f2680d1b5ab91bcc297adce62b5e81876feb
takboo <takboo.sf@gmail.com>
2025-06-11 17:51:24 +0800

    chore(script): add pre-commit script to run clippy and fmt pre-commit

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script is meant to be run as a pre-commit hook.
+# It ensures that code is formatted, linted and passes tests before committing.
+#
+# To install it, from the root of the repository, run:
+# ln -s -f ../../scripts/pre-commit.sh .git/hooks/pre-commit
+
+set -e
+
+echo "Running pre-commit hook..."
+
+echo "  cargo fmt"
+cargo fmt -- --check
+
+echo "  cargo clippy"
+SQLX_OFFLINE=true cargo clippy -- -D warnings
+
+echo "  cargo test"
+cargo test
+
+echo "Pre-commit hook passed." 
