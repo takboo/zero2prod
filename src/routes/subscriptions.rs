a492e0a4ec662100c4c2834df3e28ee12f4b67fd
takboo <takboo.sf@gmail.com>
2025-06-11 17:07:01 +0800

    chore(db): add nullable status column to subscriptions table for zero-downtime deployment

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -38,13 +38,14 @@ async fn insert_subscriber(
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
+        INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+        VALUES ($1, $2, $3, $4, $5)
         "#,
         Uuid::new_v4(),
         subscriber.email.as_ref(),
         subscriber.name.as_ref(),
-        Utc::now()
+        Utc::now(),
+        "confirmed"
     )
     .execute(pg_pool)
     .await
